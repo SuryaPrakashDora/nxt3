84014b963a5d2690d2788ad9927d38be04dd2977
suryaprakashDora <prakashmadakam21@gmail.com>
2023-12-30 05:20:39 +0000

    solution

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE product(
+    productId INT PRIMARY KEY AUTO_INCREMENT,
+    productName VARCHAR(255),
+    price DOUBLE
+);
+
+CREATE TABLE review(
+    reviewId INT PRIMARY KEY AUTO_INCREMENT,
+    reviewContent VARCHAR(255),
+    rating INT,
+    productId INT,
+    FOREIGN KEY(productId) REFERENCES product(productId)
+);
